3ab78a52664bad4981413f0e1fd3aa0e66580903
Jamieson Olsen <jamieson@fnal.gov>
2022-07-25 16:56:39 -0500

    spy buffer change to new style BRAM disable output reg attempt to fix read errors from gbe

--- src/spy.vhd
+++ src/spy.vhd
@@ -14,6 +14,8 @@ use ieee.numeric_std.all;
 
 library unisim;
 use unisim.vcomponents.all;
+library UNIMACRO;
+use unimacro.Vcomponents.all;
 
 use work.daphne_package.all;
 
@@ -36,7 +38,7 @@ architecture spy_arch of spy is
 
     signal addr_reg: std_logic_vector(11 downto 0);
     signal dia_reg, dia_q, dia_delayed:  std_logic_vector(15 downto 0);
-    signal we_reg:   std_logic;
+    signal we_reg:   std_logic_vector(0 downto 0);
 
     type state_type is (rst, wait4trig, store, wait4done);
     signal state: state_type;
@@ -77,7 +79,7 @@ begin
 
             if (reset='1') then
                 dia_reg  <= X"0000";
-                we_reg   <= '0';
+                we_reg   <= "0";
                 addr_reg <= X"000";
                 state    <= rst;
             else
@@ -89,20 +91,20 @@ begin
                     when wait4trig =>
                         if (trig='1') then
                             state <= store;
-                            we_reg <= '1';
+                            we_reg <= "1";
                         else
                             state <= wait4trig;
-                            we_reg <= '0';
+                            we_reg <= "0";
                             addr_reg <= X"000";
                         end if;
                     when store =>
                         if (addr_reg=X"FFF") then
                             state <= wait4done;
-                            we_reg <= '0';
+                            we_reg <= "0";
                         else
                             state <= store;
                             addr_reg <= std_logic_vector(unsigned(addr_reg) + 1);
-                            we_reg <= '1';
+                            we_reg <= "1";
                         end if;
                     when wait4done =>
                         if (trig='0') then
@@ -119,38 +121,80 @@ begin
     end process fsm_proc;
 
     -- instantiate four 18k true dual port blockrams to create a single 4k x 16 buffer
-    -- port A driven by the trigger logic in this module
-    -- port B is read only, controlled by the GbE interface
+    -- port A driven by the trigger logic in this module, for writing
+    -- port B is controlled by the GbE interface, for reading
 
     genbuffer: for i in 3 downto 0 generate
 
-        ramb_inst: ramb16_s4_s4 -- TDP blockram "older style" primitive both ports are 4k x 4
+        ramb_inst: BRAM_TDP_MACRO -- true dual port blockram macro both ports are 4k x 4
         generic map (
-            SIM_COLLISION_CHECK => "NONE",
-            INIT_A => X"0",
-            INIT_B => X"0",
-            SRVAL_A => X"0",
-            SRVAL_B => X"0",
-            WRITE_MODE_A => "WRITE_FIRST",
-            WRITE_MODE_B => "WRITE_FIRST"
+            BRAM_SIZE => "18Kb", -- Target BRAM, "18Kb" or "36Kb"
+            DEVICE => "7SERIES", -- Target Device: "VIRTEX5", "VIRTEX6", "7SERIES", "SPARTAN6"
+            DOA_REG => 0, -- Optional port A output register (0 or 1)
+            DOB_REG => 0, -- Optional port B output register (0 or 1)
+            INIT_A => X"000000000", -- Initial values on A output port
+            INIT_B => X"000000000", -- Initial values on B output port
+            INIT_FILE => "NONE",
+            READ_WIDTH_A => 4,   -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
+            READ_WIDTH_B => 4,   -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
+            SIM_COLLISION_CHECK => "NONE", -- Collision check enable "ALL", "WARNING_ONLY", "GENERATE_X_ONLY" or "NONE"
+            SRVAL_A => X"000000000",   -- Set/Reset value for A port output   
+            SRVAL_B => X"000000000",   -- Set/Reset value for B port output   
+            WRITE_MODE_A => "WRITE_FIRST", -- "WRITE_FIRST", "READ_FIRST" or "NO_CHANGE"
+            WRITE_MODE_B => "WRITE_FIRST", -- "WRITE_FIRST", "READ_FIRST" or "NO_CHANGE"
+            WRITE_WIDTH_A => 4, -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
+            WRITE_WIDTH_B => 4  -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
         )
         port map(
-             clka  => clka,
-             addra => addr_reg(11 downto 0),
-             dia   => dia_reg( ((i*4)+3) downto (i*4) ),
-             ena   => we_reg,
-             ssra  => '0',
-             wea   => we_reg,
+             CLKA   => clka,
+             ADDRA  => addr_reg(11 downto 0),
+             DIA    => dia_reg( ((i*4)+3) downto (i*4) ),
+             ENA    => we_reg(0),
+             RSTA   => '0',
+             WEA    => we_reg,
+             REGCEA => '1',
+             DOA    => open,
     
-             clkb  => clkb,
-             addrb => addrb(11 downto 0),
-             dib   => "0000",
-             enb   => '1',
-             ssrb  => '0',
-             web   => '0',
-             dob   => dob( ((i*4)+3) downto (i*4) )
+             CLKB   => clkb,
+             ADDRB  => addrb(11 downto 0),
+             DIB    => "0000",
+             ENB    => '1',
+             RSTB   => '0',
+             WEB    => "0",
+             REGCEB => '1',
+             DOB    => dob( ((i*4)+3) downto (i*4) )
             );
 
     end generate;
+
+-- ramb16_sx_sy is a seriously old primitive, don't use it.
+
+--    genbuffer: for i in 3 downto 0 generate
+--        ramb_inst: ramb16_s4_s4 -- TDP blockram "older style" primitive both ports are 4k x 4
+--        generic map (
+--            SIM_COLLISION_CHECK => "NONE",
+--            INIT_A => X"0",
+--            INIT_B => X"0",
+--            SRVAL_A => X"0",
+--            SRVAL_B => X"0",
+--            WRITE_MODE_A => "WRITE_FIRST",
+--            WRITE_MODE_B => "WRITE_FIRST"
+--        )
+--        port map(
+--             clka  => clka,
+--             addra => addr_reg(11 downto 0),
+--             dia   => dia_reg( ((i*4)+3) downto (i*4) ),
+--             ena   => we_reg,
+--             ssra  => '0',
+--             wea   => we_reg,
+--             clkb  => clkb,
+--             addrb => addrb(11 downto 0),
+--             dib   => "0000",
+--             enb   => '1',
+--             ssrb  => '0',
+--             web   => '0',
+--             dob   => dob( ((i*4)+3) downto (i*4) )
+--            );
+--    end generate;
 
 end spy_arch;
